71e5f37620098c16693bd8f34697d925b63ab48d
edvinysz <edvinas.kavaliauskas@jkm.lt>
2020-04-05 12:19:24 +0300

    Change text on button click

--- app/src/main/java/com/example/fintech_lab/MainActivity.java
+++ app/src/main/java/com/example/fintech_lab/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.fintech_lab;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onbuttonClick(View view) {
+        TextView textMain = findViewById(R.id.textView);
+        textMain.setText(R.string.main_text);
     }
 }
